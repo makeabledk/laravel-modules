f70e73c0d9d5ea122b5c11b79cd3a0e946fe857a
Rasmus Nielsen <rasmus@easify.dk>
2024-01-19 15:49:25 +0100

    Apply fixes from StyleCI (#12)

--- src/Commands/CreateSiteCommand.php
+++ src/Commands/CreateSiteCommand.php
@@ -7,25 +7,10 @@ use Makeable\LaravelModules\Module;
 
 class CreateSiteCommand extends CreateModuleCommand
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'modules:site {name} {--no-update}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Create a new site module';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
         $this->create(Module::site($this->argument('name')));
